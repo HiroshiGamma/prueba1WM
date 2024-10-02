686eca98f7fdf24cd3a6d5f3b2deb9a8424e9f77
HiroshiGamma <thenarux200@gmai.com>
2024-10-02 20:01:14 -0300

    feat: add user

---
+++ src/models/User.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.src.models
+{
+    public class User
+    {
+        public int Id {get;set;}
+
+        public String Rut {get;set;} = string.Empty;
+
+        public String Correo {get;set;} = string.Empty;
+
+        public String Genero {get;set;} = string.Empty;
+
+        public String FechaNacimiento {get;set;} = string.Empty;
+
+        
+    }
+}
